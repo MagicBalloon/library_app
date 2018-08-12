74e0e5b8e22a6e22363293f46da35ef14b48d336
Red Spider <d.byriansky@gmail.com>
2018-08-12 12:41:55 +0300

    Create table Categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_204306) do
+ActiveRecord::Schema.define(version: 2018_08_12_093623) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -64,6 +64,10 @@ ActiveRecord::Schema.define(version: 2018_08_11_204306) do
     t.datetime "updated_at", null: false
     t.boolean "published"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
   end
 
 end
