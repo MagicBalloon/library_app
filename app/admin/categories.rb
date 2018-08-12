60ec5ab244f39ff53d376be9d8226599cbe5fd28
Red Spider <d.byriansky@gmail.com>
2018-08-12 16:52:04 +0300

    Add multiple selection of books on the category edit page

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Category do
-  permit_params :title
+  permit_params :title, :book_ids => []
+
+  form do |f|
+    f.inputs 'Details' do
+      input :title
+      input :book_ids, :label => 'Books', :as => :select, multiple: true, :collection => Book.all.map{|a| ["#{a.title}", a.id]}
+    end
+    f.actions
+  end
 end
