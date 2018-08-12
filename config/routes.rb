d942c8b5417ce6973026a68a84198418d6133882
Red Spider <d.byriansky@gmail.com>
2018-08-12 17:49:06 +0300

    Add home page (contoller and view), change root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :authors
+  root 'home#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :books
+  resources :authors
 end
