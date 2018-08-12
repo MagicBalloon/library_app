18574e54921331b3e89538b10340b8ead278577b
Red Spider <d.byriansky@gmail.com>
2018-08-12 16:55:29 +0300

    Add books list on the show category page

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,6 +1,19 @@
 ActiveAdmin.register Category do
   permit_params :title, :book_ids => []
 
+  show do
+    attributes_table do
+      row :title
+      row 'Books' do
+        ul do
+          category.books.map do |b|
+            li link_to b.title, admin_book_path(b)
+          end
+        end
+      end
+    end
+  end
+
   form do |f|
     f.inputs 'Details' do
       input :title
