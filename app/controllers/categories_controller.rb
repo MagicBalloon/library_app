bff49561929e35d60af368b2f949d79a4e9d7da1
Red Spider <d.byriansky@gmail.com>
2018-08-14 11:11:07 +0300

    Add category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < InheritedResources::Base
   before_action :set_category, only: [:show]
 
+  def show
+    @books = @category.books
+  end
+
   private
     def set_category
       @category = Category.find(params[:id])
